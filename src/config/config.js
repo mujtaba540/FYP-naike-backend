80b7f41634be397f3bb992127221d3ff68da282a
mujtaba <mujtaba@salesassist.pk>
2021-09-22 12:18:21 +0500

    done

--- src/config/config.js
+++ src/config/config.js
@@ -23,6 +23,7 @@ const { Sequelize } = require("sequelize");
 module.exports=new Sequelize(process.env.DB_DATABASE,process.env.DB_USER,process.env.DB_PASSWORD,{
     host:process.env.DB_HOST,
     dialect:"postgres",
+    port:process.env.DB_PORT,
     define: {
         timestamps: false
     },
